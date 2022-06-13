b65fd967b7504aec94ba613394d47ae89523f515
Azim Shah <azim.1998@hotmail.com>
2022-06-13 12:23:21 +0100

    design card component visual

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
+import Card from "./components/UI/Card/Card";
 import Input from "./components/UI/Input/Input";
 
 const App = () => {
   return (
-    <Input
-      label="Label"
-      input={{
-        type: "text"
-      }}
-    />
+    <Card>
+      <Input
+        label="Label"
+        input={{
+          type: "text"
+        }}
+      />
+    </Card>
   );
 };
 
